8d95349c36d503ca86a1e8df33475639dfa18563
Dan VerWeire <dverweire@gmail.com>
2012-07-11 15:22:04 -0400

    check if redis is ready and other things

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ RedisJSONify.blacklist = ["info"];
 
 function RedisJSONify (redis) {
     //save a reference to the real send_command method
-    redis.__send_command = redis.send_command;
-    
+    redis.__send_command__ = redis.send_command;
+
     //define the send_command proxy method
     redis.send_command = function (command, args, callback) {
         //don't do json stuff on blacklisted commands
-        if (~RedisJSONify.blacklist.indexOf(command)) {
-            return redis.__send_command.apply(redis, arguments);
+        if (!this.ready || ~RedisJSONify.blacklist.indexOf(command)) {
+            return redis.__send_command__.apply(redis, arguments);
         }
-        
+
         if (!callback) {
             last_arg_type = typeof args[args.length - 1];
             if (last_arg_type === "function" || last_arg_type === "undefined") {
                 callback = args.pop();
             }
         }
-        
+
         //loop through each arg converting to JSON if possible
         args.forEach(function (arg, ix) {
             //make sure the arg is not a buffer
-            if (!arg instanceof Buffer) {
+            if (!(arg instanceof Buffer)) {
                 args[ix] = JSON.stringify(arg);
             }
         });
-        
+
         //call the real send_command method
-        redis.__send_command(command, args, function (err, result) {
+        redis.__send_command__(command, args, function (err, result) {
             if (Array.isArray(result)) {
                 //loop through each array element
                 result.forEach(function (value, ix) {
@@ -48,10 +48,10 @@ function RedisJSONify (redis) {
                         , command, result);
                 }
             }
-            
+
             return callback(err, result);
         });
     };
-    
+
     return redis;
 }
