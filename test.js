0a02570e01f300aafc739dd857d47e66572f4234
Dan VerWeire <dverweire@gmail.com>
2016-01-25 23:19:29 -0500

    Update tests using tape

--- test.js
+++ test.js
@@ -1,32 +1,69 @@
 var redis = require("redis")
-	, assert = require("assert")
-	, client = require("./")(redis.createClient())
-	, key = "test-key-asdf"
-	, val = { foo : "bar" }
-	, success = true
-	;
+    , test = require("tape")
+    ;
 
-client.set(key, val, function (err ,data) {
+test('client gets and sets string key', function (t) {
+    var client = require("./")(redis.createClient())
+        , key = "test-key-asdf"
+        , val = { foo : "bar1" }
+        ;
 
-	client.get(key, function (err, data) {
-		try {
-			assert.deepEqual(val, data);
-		}
-		catch (e) {
-			console.error(e);
-			
-			success = false;
-		}
-		
-		client.quit(function () {
-			if (success) {
-				console.error("Success.");
-				process.exit(0);
-			}
-			else {
-				console.error("Fail.");
-				process.exit(1);
-			}
-		});
-	});
+    t.plan(3);
+
+    client.set(key, val, function (err, data) {
+        t.error(err, 'no error returned on set');
+
+        client.get(key, function (err, data) {
+            t.error(err, 'no error returned on get');
+            t.deepEqual(val, data);
+            
+            client.quit(function () {
+                t.end();
+            });
+        });
+    });
+});
+
+test('client gets and sets string key even if it is an object when not using jsonKey', function (t) {
+    var client = require("./")(redis.createClient())
+        , key = { a : "test-key-asdf" }
+        , val = { foo : "bar2" }
+        ;
+
+    t.plan(3);
+
+    client.set(key, val, function (err, data) {
+        t.error(err, 'no error returned on set');
+
+        client.get("[object Object]", function (err, data) {
+            t.error(err, 'no error returned on get');
+            t.deepEqual(val, data);
+            
+            client.quit(function () {
+                t.end();
+            });
+        });
+    });
+});
+
+test('client sets and gets a json key', function (t) {
+    var client = require("./")(redis.createClient(), { jsonKey : true })
+        , key = { b : "test-key-asdf" }
+        , val = { foo : "bar3" }
+        ;
+
+    t.plan(3);
+
+    client.set(key, val, function (err ,data) {
+        t.error(err, 'no error returned on set');
+
+        client.get(key, function (err, data) {
+            t.error(err, 'no error returned on get');
+            t.deepEqual(val, data);     
+            
+            client.quit(function () {
+                t.end();            
+            });
+        });
+    });
 });
